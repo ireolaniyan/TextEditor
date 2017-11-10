dfea6d66f459dbce983c2e331e32cb50d103b60c
IreOlaniyan <ireolaniyan7@gmail.com>
2017-11-10 13:09:43 +0100

    initial commit

---
+++ test/application/MainAppTest.java
@@ -0,0 +1,49 @@
+package application;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by ire on 7/31/17.
+ */
+class MainAppTest {
+    @Test
+    void start() {
+    }
+
+    @Test
+    void showTextProApp() {
+    }
+
+    @Test
+    void showInputErrorDialog() {
+    }
+
+    @Test
+    void showLoadFileDialog() {
+    }
+
+    @Test
+    void showEditDistanceDialog() {
+    }
+
+    @Test
+    void showEDResult() {
+    }
+
+    @Test
+    void showMarkovDialog() {
+    }
+
+    @Test
+    void showLoadStage() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void getStage() {
+    }
+
+}
